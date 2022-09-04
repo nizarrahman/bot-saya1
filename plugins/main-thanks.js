2f22373c6d1b2785c5e9bf6bf1567cc5c1bd0bb7
nizarrahman <102470420+nizarrahman@users.noreply.github.com>
2022-09-04 08:51:19 +0700

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/1240fb4a4d600fb02355b.png', m, { packname: "Sama Sama by", author: "Tio" })
+conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/1240fb4a4d600fb02355b.png', m, { packname: "Sama Sama by", author: "Nizar" })
 }
 
 handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
